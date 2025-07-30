15f7f4b5a929e2c87804aa0f31ca4ff3a3597d80
Tashreef Muhammad <tashreef.muhammad@gmail.com>
2025-07-31 00:59:00 +0600

    Completed research profile section with animation
    Additionally made hamburger in navigation tab, and handled image overriding the about me text.

--- JS/programs.js
+++ JS/programs.js
@@ -4,3 +4,15 @@ function toggleCategory(header) {
     category.classList.toggle('expanded');
   }
 }
+
+function enableAutoScrollIfNeeded() {
+  const container = document.querySelector('.profile-scroll-container');
+  if (container.scrollWidth > container.clientWidth) {
+    container.classList.add('auto-scroll');
+  } else {
+    container.classList.remove('auto-scroll');
+  }
+}
+
+window.addEventListener('load', enableAutoScrollIfNeeded);
+window.addEventListener('resize', enableAutoScrollIfNeeded);
