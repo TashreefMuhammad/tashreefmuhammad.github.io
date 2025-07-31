aa5c5bfeeed0a9d0a70ff7804f01706ba4d61de1
Tashreef Muhammad <tashreef.muhammad@gmail.com>
2025-07-31 09:16:03 +0600

    Included CP profiles section
    Added bit of animation options for scroll bar and some UI optimization

--- JS/programs.js
+++ JS/programs.js
@@ -5,6 +5,13 @@ function toggleCategory(header) {
   }
 }
 
+  window.addEventListener('scroll', function () {
+    const scrollBar = document.getElementById('scroll-bar');
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    const progress = (window.scrollY / totalHeight) * 100;
+    scrollBar.style.width = progress + '%';
+  });
+
 function enableAutoScrollIfNeeded() {
   const container = document.querySelector('.profile-scroll-container');
   if (container.scrollWidth > container.clientWidth) {
